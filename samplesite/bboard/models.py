3c5f89f72d5e5f8f7d02a920f2f6320c91c5cee1
”Vadim <vadimka.gapon@gmail.com>
2022-03-07 22:46:43 +0600

    first steps

---
+++ samplesite/bboard/models.py
@@ -0,0 +1,29 @@
+from pyexpat import model
+from tabnanny import verbose
+from django.db import models
+from django.forms import CharField
+
+class Bb(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Товар')
+    content = models.TextField(null=True,blank=True, verbose_name='Описание')
+    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    publised = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+    
+    
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-publised']
+        
+        
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index = True, verbose_name = 'Название')
+    def __str__(self) -> str:
+        return self.name
+    
+    
+    class Meta:
+       verbose_name_plural = 'Рубрики' 
+       verbose_name = 'Рубрика'
+       ordering = ['name']
